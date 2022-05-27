98fb058fa58921fbe6b6383bb2e1585c74244277
Leonam Santos <leonam.ricardo@gmail.com>
2022-05-27 19:38:33 +0200

    Test Locale context

--- src/context/LocaleContext.spec.tsx
+++ src/context/LocaleContext.spec.tsx
@@ -1,15 +1,40 @@
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import LocaleContext, { LocaleProvider } from "./LocaleContext";
 import { LocaleType } from "./locale-config";
-import { ReactNode } from "react";
+import { ReactNode, useContext } from "react";
 
-const customRender = (children: ReactNode, locale: LocaleType) => {
-  return render(<LocaleProvider locale={locale}>{children}</LocaleProvider>);
+const TestComponent = ({ locale }: { locale: LocaleType }) => {
+  const { getLocale, setLocale } = useContext(LocaleContext);
+
+  const handleClick = () => setLocale(locale);
+
+  return (
+    <>
+      <p>{getLocale()}</p>
+      <button data-testid="test-button" onClick={handleClick}>
+        ChangeLocale
+      </button>
+    </>
+  );
 };
 
 describe("LocaleContext", () => {
+  const customRender = (initialLocale: LocaleType, children: ReactNode) => {
+    return render(
+      <LocaleProvider locale={initialLocale}>{children}</LocaleProvider>
+    );
+  };
+
   it("WHEN rendered", () => {
-    const { container } = customRender("", "de-DE");
+    const { container } = customRender(
+      "de-DE",
+      <TestComponent locale={"en-US"} />
+    );
+
+    expect(container).toMatchSnapshot();
+
+    const element = screen.getByTestId("test-button");
+    fireEvent.click(element);
 
     expect(container).toMatchSnapshot();
   });
