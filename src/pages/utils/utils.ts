ebf15921f63c1d936205190cdbaec635f56b19a5
Leonam Santos <leonam.ricardo@gmail.com>
2022-05-27 09:10:36 +0200

    Small refactoring

--- src/pages/utils/utils.ts
+++ src/pages/utils/utils.ts
@@ -1,10 +1,9 @@
 import { CartItem } from "../structures/cart.structures";
 
-const totalValue = (items: Pick<CartItem, "price">[]): number => {
-  return items.reduce(
-    (sum: number, item: Pick<CartItem, "price">) => sum + item.price,
-    0
-  );
+type WithPrice = Pick<CartItem, "price">;
+
+const totalValue = (items: WithPrice[]): number => {
+  return items.reduce((sum: number, item: WithPrice) => sum + item.price, 0);
 };
 
 const firstLetter = (str: string): string => {
