02ee3cc7490679d5b2f7205b28579924b21f91df
Leonam Santos <leonam.ricardo@gmail.com>
2022-10-05 19:24:54 +0200

    Renaming

--- src/pages/toolbar/ToolbarContent.tsx
+++ src/pages/toolbar/ToolbarContent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Caption from "../../components/Caption";
 
 const ToolbarContent = () => {
-  return <Caption variant="h6">{"Element Home Task"}</Caption>;
+  return <Caption variant="h6">{"Cart Home Task"}</Caption>;
 };
 
 export default ToolbarContent;
